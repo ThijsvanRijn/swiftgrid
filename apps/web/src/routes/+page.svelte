f1cbeefe4f75dfbd9b9aa4960c7eaa5cb49b816e
Thijs van Rijn <thijsvrijn@gmail.com>
2025-11-19 14:00:41 +0100

    chore: initialize project skeleton

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center gap-4">
+	<h1 class="text-3xl font-bold">Hello World</h1>
+	<Button>Click me</Button>
+</div>
